640d8141930c5dfeacd31bca11474e4545fc132b
emanelsayed39 <88773470+emanelsayed39@users.noreply.github.com>
2022-01-30 15:01:09 +0200

    after second review

--- js/app.js
+++ js/app.js
@@ -2,31 +2,22 @@
 
 
 
-/**
- * Define Global Variables
- * 
-*/
+
+ /**  Define Global Variables*/
 const sections=document.getElementsByTagName('section');
 const mainMenu=document.querySelector('nav');
 const menu=mainMenu.querySelector('ul');
 
-
-/**
- 
- * Start Helper Functions
-  
-*/
-//activate the responsive menu when screen is minmized
+/**activate the responsive menu when screen is minmized*/
 function showResponsiveMenue() {
     const nav = document.querySelector('.navbar__menu');
-    
-    if (nav.className === 'navbar__menu') {
+    if (nav.className === 'navbar__menu'){
         nav.className += ' responsive';
     } else {
         nav.className = 'navbar__menu';
     }
 }
-//deactivate active section and menu items
+/**deactivate active section and menu items*/
 function removeActiveClass(){
     for (const section of sections){
         section.classList.remove('your-active-class');
@@ -34,49 +25,43 @@ function removeActiveClass(){
     for (const item of menu.querySelectorAll('li a')){
        item.classList.remove('active__menu'); 
     }
-
-
 }
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-//scroll to section when its menu item is clicked 
+/**set Active class for section and menues */
+function setActiveClass(currentSection){
+    currentSection.classList.add('your-active-class');
+    const menuItems=menu.querySelectorAll('li a');
+    for (const item of menuItems){
+        if(item.id==='lnk'+ currentSection.id){
+            item.classList.add('active__menu');
+        }
+    }
+}
+/**scroll to section when its menu item is clicked */
 const goToSection=(event)=>{
     event.preventDefault();
-    const section=document.getElementById(event.target.id.substr(3));
-    section.scrollIntoView({block: "center",
-    behavior: "smooth"
-    });
+    let sectionId=event.target.id.substr(3);
+    const section=document.getElementById(sectionId);
+    section.scrollIntoView({block: "center",behavior: "smooth"});
 }
 
-//intersection Observer function ,work when new element get into the view point
-let observer = new IntersectionObserver((entries, observer) => { 
+/**intersection Observer function ,work when new element get into the view point*/
+let observer = new IntersectionObserver((entries,observer) => { 
     entries.forEach(entry => {
-           if(entry.intersectionRatio >=.3){
+        if(entry.intersectionRatio >=.3){
             removeActiveClass();
             let currentSection=entry.target;
-            currentSection.classList.add('your-active-class');
-            const menuItems=menu.querySelectorAll('li a');
-    
-            for (const item of menuItems){
-                if(item.id==='lnk'+ currentSection.id){
-                    item.classList.add('active__menu');
-                }
-            }
+            setActiveClass(currentSection);
         }
     });
 }, {threshold: '.3'});
-    
-// build the nav
-// Build menu 
+
+/** Build menu*/
 const fragement =document.createDocumentFragment();
 for (const section of sections){
     let menuItem=document.createElement('li');
-    menuItem.innerHTML=`<a id='lnk${section.id}' class='menu__link')' >${section.getAttribute("data-nav")}</a>`;
+    let id=section.id;
+    let name=section.getAttribute("data-nav");
+    menuItem.innerHTML=`<a id='lnk${id}' class='menu__link')' >${name}</a>`;
     menuItem.querySelector('a').addEventListener('click',goToSection);
     fragement.appendChild(menuItem);
     observer.observe(section);
@@ -85,11 +70,6 @@ for (const section of sections){
 
 
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
 
 
 
